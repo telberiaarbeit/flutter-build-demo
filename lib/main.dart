ad4d5c8bf3c7f860e0bedb62da5ab8a7afd9d911
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-13 12:39:08 +0200

    Update lib/main.dart via API

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello, Flutter Web!'),
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24),
+          ),
         ),
       ),
     );
