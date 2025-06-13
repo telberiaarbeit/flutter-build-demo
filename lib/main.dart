92d258eae675fc22fa71bce598cc50ebbd828c9e
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-13 10:47:29 +0200

    Update lib/main.dart via API

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const TodoApp());
+  runApp(const ColorSwitcherApp());
 }
 
-class TodoApp extends StatelessWidget {
-  const TodoApp({super.key});
+class ColorSwitcherApp extends StatelessWidget {
+  const ColorSwitcherApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Todo App')),
-        body: const TodoList(),
-      ),
+    return const MaterialApp(
+      home: ColorSwitcher(),
     );
   }
 }
 
-class TodoList extends StatefulWidget {
-  const TodoList({super.key});
+class ColorSwitcher extends StatefulWidget {
+  const ColorSwitcher({super.key});
 
   @override
-  State<TodoList> createState() => _TodoListState();
+  State<ColorSwitcher> createState() => _ColorSwitcherState();
 }
 
-class _TodoListState extends State<TodoList> {
-  final List<_TodoItem> _todos = [];
-  final TextEditingController _controller = TextEditingController();
+class _ColorSwitcherState extends State<ColorSwitcher> {
+  Color _backgroundColor = Colors.white;
 
-  void _addTodo() {
-    final text = _controller.text;
-    if (text.isNotEmpty) {
-      setState(() {
-        _todos.add(_TodoItem(text: text));
-        _controller.clear();
-      });
-    }
-  }
-
-  void _toggleDone(int index) {
+  void _changeColor(Color color) {
     setState(() {
-      _todos[index].isDone = !_todos[index].isDone;
+      _backgroundColor = color;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: _controller,
-                  decoration: const InputDecoration(
-                    labelText: 'Neue Aufgabe',
-                  ),
-                ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.add),
-                onPressed: _addTodo,
-              ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _todos.length,
-              itemBuilder: (context, index) {
-                final todo = _todos[index];
-                return ListTile(
-                  title: Text(
-                    todo.text,
-                    style: TextStyle(
-                      decoration: todo.isDone ? TextDecoration.lineThrough : null,
-                    ),
-                  ),
-                  onTap: () => _toggleDone(index),
-                );
-              },
+    return Scaffold(
+      backgroundColor: _backgroundColor,
+      appBar: AppBar(title: const Text('Farbschalter')),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _changeColor(Colors.green),
+              child: const Text('Grün'),
             ),
-          ),
-        ],
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () => _changeColor(Colors.black),
+              child: const Text('Schwarz'),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
-
-class _TodoItem {
-  final String text;
-  bool isDone;
-
-  _TodoItem({required this.text, this.isDone = false});
-}
+} 
