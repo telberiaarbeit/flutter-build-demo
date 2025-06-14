9519f8ff1ea1f1ff06296c136526c7e75b6736db
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-14 10:18:50 +0200

    Update lib/main.dart via API

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,21 @@
 import 'package:flutter/material.dart';
-void main() => runApp(...);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Hoang Test')),
+        body: const Center(
+          child: Text('Welcome to Hoang Test App!'),
+        ),
+      ),
+    );
+  }
+}
