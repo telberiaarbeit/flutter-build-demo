bc3c5af04effa9e8b314eff5ea4907263f560084
Fuchshofer Patrick <33409840+telberiaarbeit@users.noreply.github.com>
2025-06-25 18:14:21 +0200

    Update lib/main.dart via API

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,77 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const DiceRollerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DiceRollerApp extends StatelessWidget {
+  const DiceRollerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Customer1 App')),
-        body: const Center(
-          child: Text('Welcome to Customer1 App!'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const DiceHomePage(),
+    );
+  }
+}
+
+class DiceHomePage extends StatefulWidget {
+  const DiceHomePage({super.key});
+
+  @override
+  State<DiceHomePage> createState() => _DiceHomePageState();
+}
+
+class _DiceHomePageState extends State<DiceHomePage> {
+  int _diceResult = 1;
+
+  void _rollDice() {
+    setState(() {
+      _diceResult = Random().nextInt(20) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://i.imgur.com/lR5YfPZ.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                '$_diceResult',
+                style: const TextStyle(
+                  fontSize: 100,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                  shadows: [
+                    Shadow(blurRadius: 10, color: Colors.redAccent, offset: Offset(0, 0)),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  textStyle: const TextStyle(fontSize: 24),
+                ),
+                onPressed: _rollDice,
+                child: const Text('Roll D20'),
+              ),
+            ],
+          ),
         ),
       ),
     );
